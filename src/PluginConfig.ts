1c536066287922f28e1bce584409b7154bdba597
Jose Torres <jose@ultramusicfestival.com>
2025-08-27 21:43:57 -0400

    Update PluginConfig JSDocs.

--- src/PluginConfig.ts
+++ src/PluginConfig.ts
@@ -1,45 +1,66 @@
 import { FieldAccess } from 'payload';
 
 export interface PluginConfig {
-  /** Array of collection slugs to exclude */
+  /** Array of collection slugs to exclude
+   * @default []
+   */
   excludedCollections?: string[];
 
-  /** Array of global slugs to exclude */
+  /** Array of global slugs to exclude
+   * @default []
+   */
   excludedGlobals?: string[];
 
-  /** Name of the created by field, default: `createdBy` */
+  /** Name of the created by field
+   * @default 'createdBy'
+   */
   createdByFieldName?: string;
 
-  /** Name of the updated by field, default: `updatedBy` */
+  /** Name of the updated by field
+   * @default 'updatedBy'
+   */
   updatedByFieldName?: string;
 
-  /** Label for the created by field, default: `Created By` */
+  /** Label for the created by field
+   * @default 'Created By'
+   */
   createdByLabel?:
     | string
     | Record<string, string>
     | ((slug: string) => string | Record<string, string>);
 
-  /** Label for the updated by field, default: `Updated By` */
+  /** Label for the updated by field
+   * @default 'Updated By'
+   */
   updatedByLabel?:
     | string
     | Record<string, string>
     | ((slug: string) => string | Record<string, string>);
 
-  /** Whether the created by field is editable, default: false */
+  /** Whether the created by field is editable
+   * @default false
+   */
   createdByFieldEditable?: boolean | ((slug: string) => boolean);
 
-  /** Whether the updated by field is editable, default: false */
+  /** Whether the updated by field is editable
+   * @default false
+   */
   updatedByFieldEditable?: boolean | ((slug: string) => boolean);
 
-  /** Show readonly created & updated by fields in sidebar, default: `true` */
+  /** Show created & updated by fields in sidebar
+   * @default true
+   */
   showInSidebar?: boolean;
 
   /** Function that determines read access to the createdBy & updatedBy fields
+   * @default ({ req }) => Boolean(req.user)
    * @example
    * fieldAccess: ({ req: { user } }) => user.isAdmin
    */
   fieldAccess?: FieldAccess;
 
-  /** Show display field if value is undefined */
+  /** Show display field if value is undefined
+   * @default false
+   */
   showUndefinedValues?: boolean;
 }
